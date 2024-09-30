35751cda6d19268686eda9685de22696b196cf56
Namasvi <you@example.com>
2024-09-30 20:54:49 +0530

    nodemailer

---
+++ routes/user.route.js
@@ -0,0 +1,10 @@
+const { Router } = require('express');
+const { getUser, createUser, deleteUser, accountVerification } = require('../controllers/User.controller');
+
+const router = Router()
+router.get('/', getUser)
+router.post("/", createUser)
+router.delete('/:id', deleteUser)
+router.get("/verify/:id/:otp",accountVerification )
+
+module.exports={userRouter:router}
